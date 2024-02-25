ebb44e60f0947d0e7146eb70b8a97dc576b9d750
LelBit <110826587+Lel020@users.noreply.github.com>
2024-02-25 15:50:31 +1100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,41 @@
+class TodoApplication:
+
+    def __init__(self):
+        self.todoList = []
+
+    def addTask(self):
+        taskToAdd = input("Enter a task to complete: ")
+        self.todoList.append(taskToAdd)
+        print(f"Added {taskToAdd}")
+
+    def removeTask(self):
+        taskToRemove = input("Enter a task to remove: ")
+        self.todoList.remove(taskToRemove)
+        print(f"Removed {taskToRemove}")
+
+    def listItems(self):
+        for i in self.todoList:
+            print(i)
+
+    def exit(self):
+        print("Exiting...")
+        TodoApplication().exit()
+
+
+start = TodoApplication()
+
+while True:
+    action = input("What would you like to do.\nAdd | Remove | List | Exit: ")
+
+    if action == "Add" or action == "add":
+        start.addTask()
+        continue
+    elif action == "Remove" or action == "remove":
+        start.removeTask()
+        continue
+    elif action == "Exit" or action == "exit":
+        exit()
+    elif action == "List" or action == "list":
+        start.listItems()
+    else:
+        continue
